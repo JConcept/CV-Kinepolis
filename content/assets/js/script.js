a7fdfb53fcd3d8fbd7f37f1f2435a9877827be0b
Jean-Christophe Lepot <github@jconcept.be>
2016-11-10 05:40:35 +0100

    First upload
    Uploading project files

---
+++ content/assets/js/script.js
@@ -0,0 +1,6 @@
+function play() {
+  document.getElementById("video1").play();
+  document.getElementById("video1").controls="controls";
+  document.getElementById("playButton1").className = "";
+  document.getElementById("playButton1").innerHTML = "";
+}
